3d171f2e7b89f39126e674428a368a3a385f09eb
Guillem Ferrer <guillemfd@gmail.com>
2022-05-06 00:46:45 +0200

    adding objects

---
+++ objetos/objetos.js
@@ -0,0 +1,10 @@
+let myself = {
+    name: 'Guillem',
+    age: 37,
+    kids: ['Bernat', 'Riu']
+}
+
+console.log(myself)
+console.log(myself.name)
+
+// veig codi posant "Salida" i a la pestanya "Code". He de clicar al "play" de dalt a la dreta
